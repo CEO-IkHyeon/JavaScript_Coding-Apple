4004c027979f21bd27aec8333d9f4300dd817e2e
IkHeyonCho <andantej281@gmail.com>
2024-01-24 23:52:51 +0900

    1. tag안에 data-id로 정보 몰래 넣기 2. 이벤트 버블링 활용해 이벤트 리스너 1개만 써서 개발하기

--- tab.js
+++ tab.js
@@ -1,44 +1,26 @@
 var tabButton = $('.tab-button');
 var tabContent = $('.tab-content');
-// var divLength = document.querySelectorAll('.tab-button').length;
 var divLength = $('.tab-button').length;
-console.log(divLength);
-// tabButton[0].addEventListener('click', function() {
-//     // 한번에 모든 tab-button에 붙은 orange class와 모든 tab-content에 붙은 show class 제거
-//     tabButton.removeClass('orange');
-//     tabContent.removeClass('show');
 
-//     tabButton[0].classList.add('orange');
-//     tabContent[0].classList.add('show');
-// });
+// for (let i = 0; i < divLength; i++) {
+//     tabButton[i].addEventListener('click', function() {
+//         openTab(i);
+//     });
+// }
 
-// // $('.tab-button').eq(0).on('click', function() {
+// 이렇게 상위 요소에만 이벤트리스너를 붙이고 e.target으로 하위 요소들 감지할 수 있다
+// 이러면 이벤트리스너의 수를 줄일 수 있어 RAM 절약 가능
 
-// // })
-
-// tabButton[1].addEventListener('click', function() {
-//     tabButton.removeClass('orange');
-//     tabContent.removeClass('show');
-
-//     tabButton[1].classList.add('orange');
-//     tabContent[1].classList.add('show');
-// });
-
-// tabButton[2].addEventListener('click', function() {
-//     tabButton.removeClass('orange');
-//     tabContent.removeClass('show');
-
-//     tabButton[2].classList.add('orange');
-//     tabContent[2].classList.add('show');
-// });
+$('.list').click(function(e) {
+    openTab(parseInt(e.target.dataset.id));   //tag에 data 넣어놓을 수 있음
+});
 
 
-for (let i = 0; i < divLength; i++) {
-    tabButton[i].addEventListener('click', function() {
-        tabButton.removeClass('orange');
-        tabContent.removeClass('show');
-    
-        tabButton[i].classList.add('orange');
-        tabContent[i].classList.add('show');
-    });
+function openTab(i) {
+    tabButton.removeClass('orange');
+    tabContent.removeClass('show');
+
+    tabButton[i].classList.add('orange');
+    tabContent[i].classList.add('show');
 }
+
