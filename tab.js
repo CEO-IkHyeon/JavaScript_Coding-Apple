18ba1ddd02f6ae989839b0fe7ab701e4b7807f27
IkHeyonCho <andantej281@gmail.com>
2024-01-24 23:05:25 +0900

    tab 구현 -jQuery로 같은 class들 한번에 변경하기 -for문 공부 -div 아래 몇 개 요소 있는지 확인하는 코드

--- tab.js
+++ tab.js
@@ -1,28 +1,44 @@
+var tabButton = $('.tab-button');
+var tabContent = $('.tab-content');
+// var divLength = document.querySelectorAll('.tab-button').length;
+var divLength = $('.tab-button').length;
+console.log(divLength);
+// tabButton[0].addEventListener('click', function() {
+//     // 한번에 모든 tab-button에 붙은 orange class와 모든 tab-content에 붙은 show class 제거
+//     tabButton.removeClass('orange');
+//     tabContent.removeClass('show');
 
-document.querySelectorAll('.tab-button')[0].addEventListener('click', function() {
-    $('.tab-button').removeClass('orange');
-    $('.tab-content').removeClass('show');
+//     tabButton[0].classList.add('orange');
+//     tabContent[0].classList.add('show');
+// });
 
-    document.querySelectorAll('.tab-button')[0].classList.add('orange');
-    document.querySelectorAll('.tab-content')[0].classList.add('show');
-});
+// // $('.tab-button').eq(0).on('click', function() {
 
-// $('.tab-button').eq(0).on('click', function() {
+// // })
 
-// })
+// tabButton[1].addEventListener('click', function() {
+//     tabButton.removeClass('orange');
+//     tabContent.removeClass('show');
 
-document.querySelectorAll('.tab-button')[1].addEventListener('click', function() {
-    $('.tab-button').removeClass('orange');
-    $('.tab-content').removeClass('show');
+//     tabButton[1].classList.add('orange');
+//     tabContent[1].classList.add('show');
+// });
 
-    document.querySelectorAll('.tab-button')[1].classList.add('orange');
-    document.querySelectorAll('.tab-content')[1].classList.add('show');
-});
+// tabButton[2].addEventListener('click', function() {
+//     tabButton.removeClass('orange');
+//     tabContent.removeClass('show');
 
-document.querySelectorAll('.tab-button')[2].addEventListener('click', function() {
-    $('.tab-button').removeClass('orange');
-    $('.tab-content').removeClass('show');
+//     tabButton[2].classList.add('orange');
+//     tabContent[2].classList.add('show');
+// });
 
-    document.querySelectorAll('.tab-button')[2].classList.add('orange');
-    document.querySelectorAll('.tab-content')[2].classList.add('show');
-});
+
+for (let i = 0; i < divLength; i++) {
+    tabButton[i].addEventListener('click', function() {
+        tabButton.removeClass('orange');
+        tabContent.removeClass('show');
+    
+        tabButton[i].classList.add('orange');
+        tabContent[i].classList.add('show');
+    });
+}
