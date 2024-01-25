0ab4b7e58cf4c69d5ec775061140596b8dd1c24d
IkHeyonCho <andantej281@gmail.com>
2024-01-25 17:28:10 +0900

    forEach로 array, obj의 data 다 꺼내기

--- tab.js
+++ tab.js
@@ -25,26 +25,47 @@ function openTab(i) {
 }
 
 
+var pants = [28, 30, 32, 34];
+var shirts = [95, 100, 105];
+
+
 var formSelect = $('.form-select');
 formSelect.eq(0).on('input', function() {
     if (this.value == '셔츠') { // this : 현재 이벤트리스너가 달린 곳
         formSelect.eq(1).removeClass('form-hide');
+        formSelect.eq(1).html('');
+
+        shirts.forEach(function(data) {
+            formSelect.eq(1).append(`<option>${data}</option>`);
+        });
         
-        var shirts = `<option>95</option>
-        <option>100</option>`;
-        formSelect.eq(1).html(shirts);
 
     } else if (this.value == '바지') {
         formSelect.eq(1).removeClass('form-hide');
 
-        var pants = `<option>28</option>
-        <option>30</option>`;
-        formSelect.eq(1).html(pants);
+        formSelect.eq(1).html('');
+        pants.forEach(function(data, i) {
+            formSelect.eq(1).append(`<option>${data}</option>`);
+        });
+
+        // arrow function : 함수 안의 this 값이 달라질 수 있음(외부에서 선언한 this를 가져와 사용)
+        // pants.forEach((a) => {
+        //     formSelect.eq(1).append(`<option>${data}</option>`);
+        // })
 
     } else {
         formSelect.eq(1).addClass('form-hide');
     }
 });
+
+
+var obj = {name: 'kim', age: 20};
+
+for (var key in obj) {
+    console.log(key);
+    console.log(obj[key]);
+
+}
 
 
 
